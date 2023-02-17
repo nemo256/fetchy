2ca4303f8bb1d36b4408f13f17866e88b09dd783
Amine Neggazi <neggazimedlamine@gmail.com>
2023-02-18 00:26:00 +0100

    updated colours on icons and removed star colours...

--- src/config.h
+++ src/config.h
@@ -1,9 +1,9 @@
-#define UserText    " USER "
-#define OsText      " OS "
-#define KernelText  " KERNEL "
-#define UptimeText  " UPTIME "
-#define ShellText   " SHELL "
-#define PackageText " PKGS "
+#define UserText    BRED     "" BBLUE " USER "
+#define OsText      BGREEN   "" BBLUE " OS "
+#define KernelText  BYELLOW  "" BBLUE " KERNEL "
+#define UptimeText  BMAGENTA "" BBLUE " UPTIME "
+#define ShellText   BCYAN    "" BBLUE " SHELL "
+#define PackageText BWHITE   "" BBLUE " PKGS "
 
 /* Those two options are mutually exclusive.
  * Either set one to 'true' or both to 'false'. */
@@ -13,7 +13,7 @@
 /* See options in colour.h file */
 #define TextColour INDIGO
 
-#define PrintColours    true /* must be either 'true' or 'false' */
+#define PrintColours    false /* must be either 'true' or 'false' */
 #define ColourCharacter "󰓐"
 
 /* Some examples of things you may want to use for your ColourCharacter
