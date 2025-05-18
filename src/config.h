06655c21af1e8220a37b7c2e2291f83a91baf59c
Amine Neggazi <neggazimedlamine@gmail.com>
2025-05-18 11:21:51 +0100

    update fetchy config!

--- src/config.h
+++ src/config.h
@@ -1,9 +1,9 @@
-#define UserText    BRED     "" BBLUE " USER "       // U+F007
-#define OsText      BGREEN   "" BBLUE " OS "         // U+F303 (Arch Linux logo)
-#define KernelText  BYELLOW  "" BBLUE " KERNEL "     // U+F2DB
-#define UptimeText  BMAGENTA "" BBLUE " UPTIME "     // U+F017
-#define ShellText   BCYAN    "" BBLUE " SHELL "      // U+F489
-#define PackageText BWHITE   "" BBLUE " PKGS "       // U+F187
+#define UserText    BRED     "" BBLUE "  USER "       // U+F007
+#define OsText      BGREEN   "" BBLUE "  OS "         // U+F303 (Arch Linux logo)
+#define KernelText  BYELLOW  "" BBLUE "  KERNEL "     // U+F2DB
+#define UptimeText  BMAGENTA "" BBLUE "  UPTIME "     // U+F017
+#define ShellText   BCYAN    "" BBLUE "  SHELL "      // U+F489
+#define PackageText BWHITE   "" BBLUE "  PKGS "       // U+F187
 
 /* Those two options are mutually exclusive.
  * Either set one to 'true' or both to 'false'. */
